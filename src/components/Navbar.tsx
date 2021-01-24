8eeeb9eace444457c1593a32ccb6d56eb71aa969
sujamess <surarerk.boonjit@gmail.com>
2021-01-25 03:33:35 +0700

    Change logo from text to webp

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,22 @@
 import Link from 'next/link';
-
-const LOGO_TEXT = '<sujames />';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
     <header className="text-gray-600 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <div className="container mx-auto flex flex-wrap pl-5 flex-col md:flex-row items-center justify-center">
         <Link href="/">
-          <a className="title-font font-medium text-gray-600 hover:text-blue-600 ml-3 text-xl">
-            <span>{LOGO_TEXT}</span>
+          <a>
+            <div className="logo-wrapper">
+              <Image
+                alt="Sujames Blog Logo"
+                objectFit="contain"
+                src="https://images.ctfassets.net/ucagv55u4mg1/2Kqj6ixBp1RgtsSnXCRS3L/cf89f3734150f73426db4fd2e502093d/Untitled_Artwork.webp"
+                width={2048}
+                height={2048}
+                layout="responsive"
+              />
+            </div>
           </a>
         </Link>
       </div>
