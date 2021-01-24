22577a91e47b76cdcfdb0f386b0716c6808ac41b
sujamess <surarerk.boonjit@gmail.com>
2021-01-24 21:59:41 +0700

    Change from cyan to blue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <Link href="/">
-          <a className="title-font font-medium text-gray-600 hover:text-cyan-600 ml-3 text-xl">
+          <a className="title-font font-medium text-gray-600 hover:text-blue-600 ml-3 text-xl">
             <span>{LOGO_TEXT}</span>
           </a>
         </Link>
