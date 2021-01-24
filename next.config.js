7e08ea876606682555afd91ca05410cebfd87cc7
sujamess <surarerk.boonjit@gmail.com>
2021-01-24 18:32:51 +0700

    Edit NextJS config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,39 @@
 const withPlugins = require('next-compose-plugins')
 const withPWA = require('next-pwa');
 const withOffline = require('next-offline');
+const withPreact = require('next-plugin-preact')
 
 module.exports = withPlugins(
   [
     [withOffline],
-    [withPWA, {
-      pwa: {
-        dest: 'public',
-      },
-    }],
+    [withPWA,
+      {
+        pwa: {
+          dest: 'public',
+        },
+      }
+    ],
+    [withPreact]
   ],
+  {
+    target: 'serverless',
+    async rewrites() {
+      return [
+        {
+          source: '/sitemap.xml',
+          destination: '/src/services/seo/sitemap',
+        },
+        {
+          source: '/robots.txt',
+          destination: '/src/services/seo/robots',
+        },
+      ]
+    },
+    images: {
+      domains: ['images.ctfassets.net'],
+    },
+    experimental: {
+      optimizeFonts: true,
+    },
+  }
 );
