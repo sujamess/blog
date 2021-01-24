cd74aede1d9913f85fa08a2a94a3c722509b17ea
sujamess <surarerk.boonjit@gmail.com>
2021-01-24 18:36:27 +0700

    Add constant

---
+++ src/shared/constants/error.constant.ts
@@ -0,0 +1,4 @@
+export const INTERNAL_SERVER_ERROR_STATUS_CODE = 500;
+export const INTERNAL_SERVER_ERROR_MESSAGE = 'Internal server error';
+
+export const NOT_FOUND_ERROR_MESSAGE = 'Blog not found';
