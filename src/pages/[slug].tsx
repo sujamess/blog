ff21c0bbcef6202670c15380b07e079864d39ba7
sujames <surarerk.boonjit@gmail.com>
2021-08-04 19:06:10 +0700

    Adjust slug card

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -38,21 +38,20 @@ const Blog = ({ blog }: IBlogProps) => {
         description={blog.description}
       />
       <div className="mx-auto w-full lg:w-1/2 shadow-2xl rounded-2xl px-4 pb-4 lg:px-12 lg:pb-12 xl:px-16 xl:pb-16">
-        <div className="px-4">
-          <Image
-            alt={`${blog.title}-slug-cover-img`}
-            objectFit="contain"
-            src={blog.coverImg.url}
-            width={868}
-            height={468}
-            layout="responsive"
-          />
+        <Image
+          className="px-4"
+          alt={`${blog.title}-slug-cover-img`}
+          objectFit="contain"
+          src={blog.coverImg.url}
+          width={592}
+          height={320}
+          layout="responsive"
+        />
+        <div className="p-6">
           <Tags tags={blog.tags} />
-          <p className="text-4xl font-medium break-words">{blog.title}</p>
+          <p className="text-3xl font-medium break-words">{blog.title}</p>
           <p className="text-sm font-extralight">{blog.createdAt}</p>
           <Divider />
-        </div>
-        <div className="px-4 py-4">
           <article
             className="prose prose-lg max-w-full"
             dangerouslySetInnerHTML={{
@@ -66,9 +65,9 @@ const Blog = ({ blog }: IBlogProps) => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params, preview }) => {
-  const { default: dayjs } = await import ('dayjs');
-  const { getBlogBySlug } = await import ('src/services/contentful/blogs');
-  const { markdownToString } = await import ('src/lib/markdown');
+  const { default: dayjs } = await import('dayjs');
+  const { getBlogBySlug } = await import('src/services/contentful/blogs');
+  const { markdownToString } = await import('src/lib/markdown');
 
   try {
     const blog = await getBlogBySlug({ slug: params.slug as string, preview });
@@ -90,7 +89,7 @@ export const getStaticProps: GetStaticProps = async ({ params, preview }) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const { getBlogs } = await import ('src/services/contentful/blogs');
+  const { getBlogs } = await import('src/services/contentful/blogs');
 
   const blogs = await getBlogs();
 
